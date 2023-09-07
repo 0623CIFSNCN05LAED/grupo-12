3fa3d17a409d39d2c8234111682ef540373908ec
reak97 <sebamontero15@gmail.com>
2023-09-06 21:17:27 -0300

    add files product-controller and user-controller

--- src/controllers/main-controller.js
+++ src/controllers/main-controller.js
@@ -1,58 +1,5 @@
-const catalogo= require("../data/catalogo");
-
-const bikes= catalogo.bikes;
-const clothes= catalogo.clothes;
-const accessories= catalogo.accessories;
-
 module.exports = {
   home: (req, res) => {
     res.render("home");
   },
-
-  login: (req, res) => {
-    res.render("login");
-  },
-
-  register: (req, res) => {
-    res.render("register");
-  },
-
-  productCart: (req, res) => {
-    res.render("product-cart");
-  },
-
-
-  productDetailBikes: (req, res) =>{
-    const id = req.params.id;
-    const bike= bikes.find((bike) => bike.id == id);
-    res.render("product-detail-bikes", {'bike' : bike});
-  },
-
-  productDetailClothes: (req, res) =>{
-    const id = req.params.id;
-    const cloth= clothes.find((cloth) => cloth.id == id);
-    res.render("product-detail-clothes", {'cloth' : cloth});
-  },
-
-  productDetailAccessories: (req, res) =>{
-    const id = req.params.id;
-    const accessory= accessories.find((accessory) => accessory.id == id);
-    res.render("product-detail-accessories", {'accessory' : accessory});
-  },
-
-  productListBikes: (req, res) =>{
-    const id= req.params.id;
-    res.render("product-list-bikes", {bycicles : bikes } );
-  },
-
-  productListClothes: (req, res) =>{
-    const id= req.params.id;
-    res.render("product-list-clothes" , {clothing : clothes } );
-  },
-
-  productListAccessories: (req, res) =>{
-    const id= req.params.id;
-    res.render("product-list-accessories", {accessories : accessories } );
-  },
-
 };
--- src/routes/main-router.js
+++ src/routes/main-router.js
@@ -1,18 +1,20 @@
 const { Router } = require("express");
 
+const productController = require("../controllers/product-controller");
+const userController = require("../controllers/user-controller");
 const mainController = require("../controllers/main-controller");
 
 const router = Router();
 
 router.get("/", mainController.home);
-router.get("/login", mainController.login);
-router.get("/bikes", mainController.productListBikes); 
-router.get("/bikes/:id", mainController.productDetailBikes);
-router.get("/clothes", mainController.productListClothes)
-router.get("/clothes/:id", mainController.productDetailClothes);
-router.get("/accessories", mainController.productListAccessories);
-router.get("/accessories/:id", mainController.productDetailAccessories);
-router.get("/register", mainController.register)
-router.get("/product-cart", mainController.productCart)
+router.get("/login", userController.login);
+router.get("/bikes", productController.productListBikes); 
+router.get("/bikes/:id", productController.productDetailBikes);
+router.get("/clothes", productController.productListClothes)
+router.get("/clothes/:id", productController.productDetailClothes);
+router.get("/accessories", productController.productListAccessories);
+router.get("/accessories/:id", productController.productDetailAccessories);
+router.get("/register", userController.register)
+router.get("/product-cart", productController.productCart)
 
 module.exports = router;
