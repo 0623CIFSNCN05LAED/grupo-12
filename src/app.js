4cce8937e17fc9f79e9a31b4990b8ea44b4eb7ba
Eugenia Ruiz <meruiz0203@gmail.com>
2023-09-20 17:43:36 -0300

    agregando nueva rama

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const methodOverride = require('method-override')
-
+const session = require(express.session);
 const app = express(); 
 
 app.use(express.static(path.join(__dirname,"../public")));
