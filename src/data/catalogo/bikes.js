7e8f8549d3baf2e79d4ca47a45d787fb5d30f962
ibocian <arqibocian@gmail.com>
2023-10-14 02:10:16 -0300

    RESUELTO Issue #29 en users y catalogo

--- src/data/catalogo/bikes.js
+++ src/data/catalogo/bikes.js
@@ -1,7 +1,8 @@
-const { log } = require("console");
+
 const fs = require("fs");
 const path = require("path");
 const { v4: uuidv4 } = require("uuid");
+
 
 module.exports = {
     getBikes: function () {
--- src/data/users/users.js
+++ src/data/users/users.js
@@ -1,4 +1,4 @@
-const { log } = require("console");
+
 const fs = require("fs");
 const path = require("path");
 const { v4: uuidv4 } = require("uuid");
