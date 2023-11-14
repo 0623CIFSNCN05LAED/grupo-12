ab560edc08418a8c5bb2e7e93cea54372152bbfe
LuisMariaLucero <luismarialucero@gmail.com>
2023-11-14 14:58:46 -0300

    data base

---
+++ src/database/models/products/products.js
@@ -0,0 +1,13 @@
+const bikes = require("./models/products/bikes");
+const brands = require("./models/products/brands");
+const categories =  require("./models/products/categories");
+const colors  = require("./models/products/colors");
+const ModelsByBrand = require("./models/products/model-by-brand");
+const sizes = require("./models/products/sizes");
+
+
+
+module.exports = {
+    
+
+}
