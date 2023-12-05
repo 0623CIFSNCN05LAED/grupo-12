a2aff5382a2628a9a6f7b008c790405b2b8435c9
meruiz <meruiz0203@gmail.com>
2023-12-05 15:43:27 -0300

    viculacion en app.jsx de dashboard

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Dashboard from "./components/Dashboard";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <header>
+        <Dashboard />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import "./Dashboard.css";
+//import logo from "./logo.png";
+
+
+function Dashboard () {
+return (
+    <div className="dashboard">
+
+    <h1> Probando conección de componentes dashboard </h1>
+
+    </div>
+);
+
+}
+
+
+export default Dashboard;
